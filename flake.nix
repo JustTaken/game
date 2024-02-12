5e405dd6dff1de8246d7e166238e14d32a5dd793
JustTaken <juub_camara@hotmail.com>
2024-02-12 18:21:36 -0400

    Add nix flakes support

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Vulkan application in zig";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        glfw
+        lldb
+        vulkan-loader
+        vulkan-headers
+        pkg-config
+        shaderc
+        emacs29
+
+        (callPackage ./nix-builds/zig.nix {})
+      ];
+    };
+  };
+}
